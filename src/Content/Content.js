1a2854fc880b6cef5ce84041057d3c7da2be8e13
Alena1508 <alena.makarova1508@gmail.com>
2018-04-03 18:42:06 +0300

    Start with homework 17

--- src/Content/Content.js
+++ src/Content/Content.js
@@ -1,73 +1,17 @@
 import './content.scss';
 
-import { Counter } from './Counter';
+// import { Counter } from './Counter';
+import {Form} from '../Form'
+import {Timer} from "../Timer";
 
 
-const Greeting = ({ name }) => {
-  const hour = new Date().getHours();
-  let partDay = '';
+stopTimer = () => {
 
-  if (hour >= 3 && hour < 12) {
-    partDay = 'morning';
-  } else if (hour >= 12 && hour < 18) {
-    partDay = 'afternoon';
-  } else if (hour >= 18 && hour < 22) {
-    partDay = 'evening';
-  }
-  partDay = 'night';
-
-  return <p>Good { partDay }, { name }!</p>;
 };
-
-const Number = ({
-  from, to, odd, even
-}) => {
-  const num = [];
-
-  for (let i = from; i < to; i++) {
-    if (odd) {
-      if ((i % 2) === 0) {
-        num.push(<li>{ i }</li>);
-      }
-    } else if (even) {
-      if (i % 2 !== 0) {
-        num.push(<li>{ i }</li>);
-      }
-    } else {
-      num.push(<li>{ i }</li>);
-    }
-  }
-  return <ul>{ num }</ul>;
-};
-
-const users = [
-  { firstName: 'Alena', lastName: 'Makarova', age: 24 },
-  { firstName: 'Andrey', lastName: 'Makarov', age: 25 },
-  { firstName: 'Eva', lastName: 'Makarova', age: 15 }
-];
-
-const User = ({ user }) => <div> {user.firstName } { user.lastName } { user.age }</div>;
-
-const UsersList = () => {
-  const list = users.map(user => <li><User user={user} /></li>);
-  return <ul>{ list }</ul>;
-};
-
-const Time = () => <time>{ new Date().toTimeString() }</time>;
-
 export const Content = () => (
   <div className="content">
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
-    </p>
-    <Time />
-    <Greeting name="Alena" />
-    <Number
-      from={5}
-      to={10}
-      odd
-    />
-    <Counter />
-    <UsersList users="users" />
+    <Form/>
+    <Timer/>
+    <button onClick={stopTimer()}>Stop</button>
   </div>
 );
---
+++ src/Timer/Timer.js
@@ -0,0 +1,42 @@
+export class Timer extends React.Component {
+  state = {
+    time: ''
+  };
+
+  constructor(props) {
+    super(props);
+    console.log('lala');
+  }
+
+  componentDidMount() {
+    setInterval(this.createTimer, 1000);
+  }
+
+  componentWillUnmount() {
+    console.log('unmount');
+    clearInterval(this.createTimer());
+  }
+
+  createTimer = () => {
+    let dateNow = new Date();
+    console.log(dateNow);
+    let time = `${dateNow.getHours()}:${dateNow.getMinutes()}:${dateNow.getSeconds()}`;
+    this.setState({
+      time
+    });
+  };
+
+
+
+
+  render() {
+    const {time} = this.state;
+    return (
+      <div>
+        <h2>Hello, user!</h2>
+        <time>{time}</time>
+      </div>
+    )
+  }
+};
+
